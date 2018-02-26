eb061e9107dad38dbe787d5f717f9146f2b78d62
hublaw <ldoireann@gmail.com>
2018-02-26 00:28:47 -0500

    [1] Coderbyte for longest word

---
+++ coderbyte/LongestWord.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+class LongestWord {
+
+  public static String longestWord(String sentence) {
+    String[] sen = sentence.replaceAll("[^a-zA-Z ]", "").split(" ");
+    String longest = sen[0];
+    for (String s : sen) {
+      System.out.println(s);
+      if (s.length() > longest.length()) {
+        longest = s;
+      }
+    }
+    return longest;
+  }
+
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.print(longestWord(s.nextLine()));
+  }
+}

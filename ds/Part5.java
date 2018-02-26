59228de918ee107883a0ea961f0dfff743ca1874
hublaw <ldoireann@gmail.com>
2018-02-26 13:27:19 -0500

    [1.5] Data Structures HashMap

---
+++ ds/Part5.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Map;
+import java.util.HashMap;
+
+/* Read the input one line at a time and, instead of outputting the line, output
+   the number of times you have already seen that line. Again, take special care
+	 so that a file with a lot of duplicate lines does not use more memory than
+	 what is required for the number of unique lines. */
+
+public class Part5 {
+
+	/**
+	 * Your code goes here - see Part0 for an example
+	 * @param r the reader to read from
+	 * @param w the writer to write to
+	 * @throws IOException
+	 */
+	public static void doIt(BufferedReader r, PrintWriter w) throws IOException {
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		for (String line = r.readLine(); line != null; line = r.readLine()) {
+			Integer freq = map.get(line);
+			map.put(line, (freq == null) ? 1 : freq + 1);
+			w.println((freq == null) ? 0 : freq);
+		}
+	}
+
+	/**
+	 * The driver.  Open a BufferedReader and a PrintWriter, either from System.in
+	 * and System.out or from filenames specified on the command line, then call doIt.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			BufferedReader r;
+			PrintWriter w;
+			if (args.length == 0) {
+				r = new BufferedReader(new InputStreamReader(System.in));
+				w = new PrintWriter(System.out);
+			} else if (args.length == 1) {
+				r = new BufferedReader(new FileReader(args[0]));
+				w = new PrintWriter(System.out);
+			} else {
+				r = new BufferedReader(new FileReader(args[0]));
+				w = new PrintWriter(new FileWriter(args[1]));
+			}
+			long start = System.nanoTime();
+			doIt(r, w);
+			w.flush();
+			long stop = System.nanoTime();
+			System.out.println("Execution time: " + 10e-9 * (stop-start));
+		} catch (IOException e) {
+			System.err.println(e);
+			System.exit(-1);
+		}
+	}
+}
